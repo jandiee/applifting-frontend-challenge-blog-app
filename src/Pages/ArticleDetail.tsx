6734ecee2108278ea2b95edbe515cbf40d753382
Jandiee <jandek.martin@gmail.com>
2022-10-15 23:37:00 +0200

    do not use mocked article in articleDetail

--- src/Pages/ArticleDetail.tsx
+++ src/Pages/ArticleDetail.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import dayjs from "dayjs";
-import * as mockedArticle from "../Assets/mockedArticles.json";
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { useParams } from "react-router-dom";
+import articleImage from "../Assets/articleImage1.jpeg";
 import CommentList from "../Components/ArticleDetail/CommentList";
 import RelatedArticles from "../Components/ArticleDetail/RelatedArticles";
 import Header from "../Components/Header";
 import SoftText from "../Components/utils/SoftText";
-import articleImage from "../Assets/articleImage1.jpeg";
-import articleContent from "../Assets/mockedArticleContent1.md";
-import ReactMarkdown from "react-markdown";
+import agent from "../Services/agent";
 
 const ArticleDetail = () => {
   const { id: articleId } = useParams();
@@ -17,27 +16,19 @@ const ArticleDetail = () => {
   });
 
   useEffect(() => {
-    // README: Wanted behaviour - I will use the mocked version for now anyway.
-
-    // (async () => {
-    //   if (articleId) {
-    // const response = await agent.Articles.detail(articleId);
-    // setArticleDetail(response);
-    //   }
-    // })();
-    // + add articleId as useEffect dependency
-    setArticleDetail(mockedArticle);
-    fetch(articleContent)
-      .then((response) => response.text())
-      .then((text) => {
-        setArticleDetail((old) => ({ ...old, content: text }));
-      });
-  }, []);
-
-  if (!articleId) {
-    // TODO: handle this situation - ?should not be possible?
-    return <div></div>;
-  }
+    (async () => {
+      if (articleId) {
+        const response = await agent.Articles.detail(articleId);
+        setArticleDetail(response);
+      }
+    })();
+    // setArticleDetail(mockedArticle);
+    // fetch(articleContent)
+    //   .then((response) => response.text())
+    //   .then((text) => {
+    //     setArticleDetail((old) => ({ ...old, content: text }));
+    //   });
+  }, [articleId]);
 
   if (Object.keys(articleDetail).length === 0) {
     // TODO: loader
@@ -50,12 +41,12 @@ const ArticleDetail = () => {
         <Header bold>{articleDetail.title}</Header>
         <SoftText>
           <div className="flex gap-2">
-            <span>{articleDetail.author}</span>
-            <span>&#x2022;</span>
+            {/* <span>{articleDetail.author}</span>
+            <span>&#x2022;</span> */}
             <span>{dayjs().format("l")}</span>
           </div>
         </SoftText>
-        <main>
+        <main className="space-y-4">
           <div className="flex justify-center">
             <img
               src={articleImage}
